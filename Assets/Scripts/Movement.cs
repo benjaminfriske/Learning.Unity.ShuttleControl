02ae75c7a637cd8711c4df008c15d6050ceb0c3c
Benjamin Friske <benjaminfriske@gmail.com>
2022-02-04 17:02:26 -0700

    Initial movement script.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float rotationSpeed = 10;
+
+    private Rigidbody playerRigidBody;
+    private void Start()
     {
-        
+        playerRigidBody =  this.gameObject.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        var axisHorizontal = Input.GetAxis("Horizontal");        
+        if (axisHorizontal != 0) 
+        {
+            if (axisHorizontal > 0) {
+            //right
+            Vector3 rotationForce =  Vector3.forward * 1 * Time.deltaTime;
+                playerRigidBody.AddRelativeForce(rotationForce);
+            }
+            
+            else {
+            //left
+                Vector3 rotationForce =  new Vector3(0, axisHorizontal * rotationSpeed *  Time.deltaTime, 0);
+                playerRigidBody.AddRelativeForce(rotationForce);
+            }
+           
+        }
     }
 }
